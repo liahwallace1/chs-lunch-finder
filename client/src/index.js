be3b15dbd14276c434f5a5f04f0a2fc499a30440
liahwallace1 <liahwallace1@gmail.com>
2017-06-15 16:49:14 -0400

    changed index.js to go to a router, started setting up routes file

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, browserHistory } from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
+import Routes from './Routes.js'
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={browserHistory} routes={Routes} />,
+  document.getElementById('root'));
 registerServiceWorker();
