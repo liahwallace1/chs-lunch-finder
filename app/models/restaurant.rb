b35396a3a9b246f23e8365d3fd72051cfa7ac8bf
liahwallace1 <liahwallace1@gmail.com>
2017-06-15 10:46:22 -0400

    updated category from type to title, updated seeds

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :restaurant_categories
   has_many :categories, through: :restaurant_categories
+  has_many :restaurant_neighborhoods
+  has_many :neighborhoods, through: :restaurant_neighborhoods
 
 end
