0f2f25cb1dc8b70d701af8e98607238979318ea1
liahwallace1 <liahwallace1@gmail.com>
2017-06-15 12:52:22 -0400

    updated restaurant controller to fix syntax errors

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -6,7 +6,12 @@ class Api::RestaurantsController < ApplicationController
   end
 
   def create
-
+    @restaurant = Restaurant.create(restaurant_params)
+    if @restaurant.save
+      render json: Restaurant.all, success: "You added a restaurant!"
+    else
+      render json: {message: "Dang! That didn't work."}, status: 412
+    end
   end
 
   def show
@@ -14,10 +19,23 @@ class Api::RestaurantsController < ApplicationController
     render json: @restaurant
   end
 
+  def destroy
+    set_restaurant
+    if @restaurant.delete
+      render json: Restaurant.all, success: "Success! That crappy restaurant is out of rotation."
+    else
+      render json: {message: "Dang! That didn't work."}, status: 412
+    end
+  end
+
   private
 
   def set_restaurant
   @restaurant ||= Restaurant.find_by_id(params[:id])
-end
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :city, :state, :phone, :price, :takeout, :delivery, :website, :yelp, neighborhood_ids: [], category_ids: [])
+  end
 
 end
