08db68bd7bf273ff31e2abdb24880d39fe8c6707
liahwallace1 <liahwallace1@gmail.com>
2017-06-21 15:10:47 -0400

    updated restaurants controller, added id to attributes in serializer

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,6 +1,5 @@
 class Api::RestaurantsController < ApplicationController
 
-
   def index
     restaurants = Restaurant.all
     render json: restaurants
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,5 +1,5 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :yelp_id, :name, :address, :city, :state, :zip_code, :phone, :display_phone, :price, :takeout, :delivery, :yelp_rating, :yelp_url, :latitude, :longitude, :categories
+  attributes :id, :yelp_id, :name, :address, :city, :state, :zip_code, :phone, :display_phone, :price, :takeout, :delivery, :yelp_rating, :yelp_url, :latitude, :longitude, :categories
 
   has_many :categories, through: :restaurant_categories
 
