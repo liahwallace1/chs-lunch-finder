9569a8d7f769b0291a28dab61e387e1107eb407b
Liahwallace1 <Liahwallace1@gmail.com>
2017-07-03 12:39:48 -0400

    added custom method for hashtags to restaurant serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,7 +1,16 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :yelp_id, :name, :address, :city, :state, :zip_code, :phone, :display_phone, :price, :takeout, :delivery, :yelp_rating, :image_url, :yelp_url, :latitude, :longitude, :categories, :category_list
-
   has_many :categories, through: :restaurant_categories
+  has_many :hashtags, through: :restaurant_hashtags
+
+  attributes :id, :yelp_id, :name, :address, :city, :state, :zip_code, :phone, :display_phone, :price, :takeout, :delivery, :yelp_rating, :image_url, :yelp_url, :latitude, :longitude, :category_list, :hashtags_list, :categories
+
+  def hashtags_list
+    list = []
+    object.hashtags.map do |hashtag|
+      list << hashtag.name
+    end
+    return list.join(", ")
+  end
 
 
 end
